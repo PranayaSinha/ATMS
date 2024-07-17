6016c796a499bd77a6bfef8c8fff44ff23a28bc4
PranayaSinha <93397150+PranayaSinha@users.noreply.github.com>
2024-07-16 17:32:13 -0700

    Temp Change

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -83,7 +83,7 @@
                 </div>
                 <!-- End Checkbox -->
 
-                <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Sign up</button>
+                <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"><router-link to="/Table">Signin</router-link></button>
                 </div>
             </form>
             <!-- End Form -->
